b86831ba6dee4ee3a481c03fe3a751f4c817aac7
leozinskygiy <leonardoroberto777990@gmail.com>
2026-09-25 10:44:03 -0300

    Delete ex15_ano_bissexto.py

--- ex15_ano_bissexto.py
+++
@@ -1,6 +0,0 @@
-ano = int(input("Digite um ano (ex: 2024): "))
-
-if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print(f"O ano {ano} é BISSEXTO.")
-else:
-    print(f"O ano {ano} NÃO é bissexto.")
